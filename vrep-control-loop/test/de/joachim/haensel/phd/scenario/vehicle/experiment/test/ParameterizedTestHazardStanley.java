6c8e1447bf4f13f0ce9ed25bb3f039c57847875f
joachim.haensel <joachim.haensel@hpi.de>
2019-02-04 21:10:36 +0100

    two more parameterized converted

--- vrep-control-loop/test/de/joachim/haensel/phd/scenario/vehicle/experiment/test/ParameterizedTestHazardStanley.java
+++ vrep-control-loop/test/de/joachim/haensel/phd/scenario/vehicle/experiment/test/ParameterizedTestHazardStanley.java
@@ -1,5 +1,6 @@
 package de.joachim.haensel.phd.scenario.vehicle.experiment.test;
 
+
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
@@ -7,14 +8,15 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
 
 import coppelia.IntWA;
 import coppelia.remoteApi;
@@ -37,32 +39,13 @@ import de.joachim.haensel.phd.scenario.vehicle.experiment.TireBlowOutAtPositionE
 import de.joachim.haensel.phd.scenario.vehicle.experiment.TrajectoryRecorder;
 import de.joachim.haensel.vrepshapecreation.VRepObjectCreation;
 
-/**
- * TODO parameterized
- * @author dummy
- *
- */
-//@RunWith(Parameterized.class)
 public class ParameterizedTestHazardStanley
 {
     private static VRepRemoteAPI _vrep;
     private static int _clientID;
     private static VRepObjectCreation _objectCreator;
     
-    private double _lookahead;
-    private double _maxVelocity;
-    private double _maxLongitudinalAcceleration;
-    private double _maxLongitudinalDecceleration;
-    private double _maxLateralAcceleration;
-    private List<Position2D> _targetPoints;
-    private Position2D _eventPosition;
-    private RoadMap _map;
-    private String _paramID;
-    private String _color;
-    private boolean[] _tiresToBlow;
-    
-//    @Parameterized.Parameters
-    public static Collection<Object[]> parameters()
+    public static Stream<Arguments> parameters()
     {
         return Arrays.asList(new Object[][]
         {
@@ -70,37 +53,9 @@ public class ParameterizedTestHazardStanley
 //            {20, 120, 6.0, 8.0, 5.0, Arrays.asList(new Position2D(3653.19, 4666.35), new Position2D(3845.60, 4744.58)), null, new boolean[]{true, false, false, false}, "test_20_120_6.0_8.0_5.0_noBlowout", "blue"},
             {20, 60, 2.0, 8.0, 5.0, Arrays.asList(new Position2D(3653.19, 4666.35), new Position2D(3845.60, 4744.58)), new Position2D(3861.07, 4705.83), new boolean[]{true, false, false, false}, "test_20_120_6.0_8.0_5.0", "red"},
             {20, 60, 2.0, 8.0, 5.0, Arrays.asList(new Position2D(3653.19, 4666.35), new Position2D(3845.60, 4744.58)), null, new boolean[]{true, false, false, false}, "test_20_120_6.0_8.0_5.0_noBlowout", "blue"},
-        });
+        }).stream().map(params -> Arguments.of(params));
     }
 
-    public ParameterizedTestHazardStanley(double lookahead, double maxVelocity, double maxLongitudinalAcceleration, double maxLongitudinalDecceleration, double maxLateralAcceleration, List<Position2D> targetPoints, Position2D eventPosition, boolean[] tiresToBlow, String paramID, String color)
-    {
-        _lookahead = lookahead;
-        _maxVelocity = maxVelocity;
-        _maxLongitudinalAcceleration = maxLongitudinalAcceleration;
-        _maxLongitudinalDecceleration = maxLongitudinalDecceleration;
-        _maxLateralAcceleration = maxLateralAcceleration;
-        RoadMapAndCenterMatrix mapAndCenterMatrix;
-        try
-        {
-            mapAndCenterMatrix = SimulationSetupConvenienceMethods.createCenteredMap(_clientID, _vrep, _objectCreator, "./res/roadnetworks/neumarkRealWorldNoTrains.net.xml");
-            _map = mapAndCenterMatrix.getRoadMap();
-            TMatrix centerMatrix = mapAndCenterMatrix.getCenterMatrix();
-            _targetPoints = targetPoints.stream().map(point -> point.transform(centerMatrix)).collect(Collectors.toList());
-            if(eventPosition != null)
-            {
-                _eventPosition = eventPosition.transform(centerMatrix);
-                _tiresToBlow = tiresToBlow;
-            }
-        }
-        catch (VRepException exc)
-        {
-            fail(exc);
-        }
-        _paramID = paramID;
-        _color = color;
-    }
-    
     @BeforeAll
     public static void setupVrep() throws VRepException
     {
@@ -114,6 +69,12 @@ public class ParameterizedTestHazardStanley
     {
         waitForRunningSimulationToStop();
         _vrep.simxFinish(_clientID);
+    }
+    
+    @AfterEach
+    public void cleanUpObjects() throws VRepException
+    {
+        _objectCreator.deleteAll();
     }
 
     private static void waitForRunningSimulationToStop() throws VRepException
@@ -133,22 +94,36 @@ public class ParameterizedTestHazardStanley
             _vrep.simxCallScriptFunction(_clientID, "ScriptLoader", remoteApi.sim_scripttype_customizationscript, "simulationState", null, null, null, null, simStatus, null, null, null, remoteApi.simx_opmode_blocking);
         }
     }
-
-    @AfterEach
-    public void cleanUpObjects() throws VRepException
-    {
-        _objectCreator.deleteAll();
-    }
-    
     
     @Test
-    public void testTireBlowOutScenario()
+    public void testTireBlowOutScenario(double lookahead, double maxVelocity, double maxLongitudinalAcceleration, double maxLongitudinalDecceleration, double maxLateralAcceleration, List<Position2D> targetPoints, Position2D eventPosition, boolean[] tiresToBlow, String paramID, String color)
     {
+        RoadMapAndCenterMatrix mapAndCenterMatrix;
+        RoadMap _map = null;
+        Position2D _eventPosition = null;
+        List<Position2D> _targetPoints = null;
+        boolean[] _tiresToBlow = null;
+        try
+        {
+            mapAndCenterMatrix = SimulationSetupConvenienceMethods.createCenteredMap(_clientID, _vrep, _objectCreator, "./res/roadnetworks/neumarkRealWorldNoTrains.net.xml");
+            _map = mapAndCenterMatrix.getRoadMap();
+            TMatrix centerMatrix = mapAndCenterMatrix.getCenterMatrix();
+            _targetPoints = targetPoints.stream().map(point -> point.transform(centerMatrix)).collect(Collectors.toList());
+            if(eventPosition != null)
+            {
+                _eventPosition = eventPosition.transform(centerMatrix);
+                _tiresToBlow = tiresToBlow;
+            }
+        }
+        catch (VRepException exc)
+        {
+            fail(exc);
+        }
         try
         {
             TaskCreator taskCreator = new TaskCreator();
             PointListTaskCreatorConfig config = new PointListTaskCreatorConfig(true);
-            config.setControlParams(_lookahead, _maxVelocity, _maxLongitudinalAcceleration, _maxLongitudinalDecceleration, _maxLateralAcceleration);
+            config.setControlParams(lookahead, maxVelocity, maxLongitudinalAcceleration, maxLongitudinalDecceleration, maxLateralAcceleration);
 
             config.setMap(_map);
             config.configSimulator(_vrep, _clientID, _objectCreator);
@@ -167,7 +142,7 @@ public class ParameterizedTestHazardStanley
                 public ILowerLayerControl create()
                 {
                     StanleyController controller = new StanleyController();
-                    controller.setParameters(new PurePursuitParameters(_lookahead, 0.0));
+                    controller.setParameters(new PurePursuitParameters(lookahead, 0.0));
                     
                     return controller;
                 }
@@ -179,12 +154,12 @@ public class ParameterizedTestHazardStanley
             executor.execute(tasks);
             System.out.println("bla");
             
-            List<String> actualTrajectory = trajectoryRecorder.getTrajectory().stream().map(te -> te.getPos()).filter(p -> p.getX() != 0 && p.getY() != 0.0).map(pos -> pos.toPyPlotString(_color)).collect(Collectors.toList());
+            List<String> actualTrajectory = trajectoryRecorder.getTrajectory().stream().map(te -> te.getPos()).filter(pos -> pos.getX() != 0.0 && pos.getY() != 0.0).map(pos -> pos.toPyPlotString(color)).collect(Collectors.toList());
             List<String> plannedTrajectory = trajectoryRecorder.getPlannedTrajectory().stream().map(pos -> pos.toPyPlotString("black")).collect(Collectors.toList());
             try
             {
-                Files.write(new File("./res/hazardtestoutput/trajectory" + _paramID + ".actual.pyplot").toPath(), actualTrajectory, Charset.defaultCharset());
-                Files.write(new File("./res/hazardtestoutput/trajectory" + _paramID + ".planned.pyplot").toPath(), plannedTrajectory, Charset.defaultCharset());
+                Files.write(new File("./res/hazardtestoutput/trajectory" + paramID + ".actual.pyplot").toPath(), actualTrajectory, Charset.defaultCharset());
+                Files.write(new File("./res/hazardtestoutput/trajectory" + paramID + ".planned.pyplot").toPath(), plannedTrajectory, Charset.defaultCharset());
             }
             catch (IOException exc)
             {
