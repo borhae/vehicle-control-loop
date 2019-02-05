c1350cbbae6d5f6fcac39bff25e340180136f6f3
joachim.haensel <joachim.haensel@hpi.de>
2019-02-05 16:33:17 +0100

    removed unused comments

--- vrep-control-loop/test/de/joachim/haensel/phd/scenario/operationalprofile/collection/TestGroundTruthCounting.java
+++ vrep-control-loop/test/de/joachim/haensel/phd/scenario/operationalprofile/collection/TestGroundTruthCounting.java
@@ -52,40 +52,6 @@ public class TestGroundTruthCounting
             List<ObservationConfiguration> obsConfs = ObservationConfiguration.create(observations, configurations); 
             Collections.sort(obsConfs);
             System.out.println("test");
-//            List<EquivalenClassEntry> equivalenceClassEntries = new ArrayList<>();
-//            Set<Long> timeStamps = configurations.keySet();
-//            for(Long curTimeStamp : timeStamps)
-//            {
-//                EquivalenClassEntry equivalenceClassEntry = new EquivalenClassEntry(curTimeStamp, configurations.get(curTimeStamp), observations.get(curTimeStamp));
-//                equivalenceClassEntries.add(equivalenceClassEntry);
-//            }
-//            
-//            List<List<Double>> allValues = new ArrayList<>();
-//            for(int idx = 0; idx < equivalenceClassEntries.size(); idx++)
-//            {
-//                EquivalenClassEntry curEntry = equivalenceClassEntries.get(idx);
-//                ICountListElem curNode = curEntry.getRoot();
-//                List<Double> valuesOfThisEntry = new ArrayList<>();
-//                int cnt = 0;
-//                while(curNode != null)
-//                {
-//                    valuesOfThisEntry.add(curNode.getNormyValue());
-//                    curNode = curNode.next();
-//                    cnt++;
-//                }
-//                if(cnt == 43)
-//                {
-//                    allValues.add(valuesOfThisEntry);
-//                }
-//            }
-//            for (List<Double> curList : allValues)
-//            {
-//                curList.remove(curList.size() - 1);
-//            }
-//            List<String> lines = new ArrayList<>();               
-//            lines.add("Velocity, Angle, Displacement, SetVelocity0, SetAngle0, SetVelocity1, SetAngle1, SetVelocity2, SetAngle2, SetVelocity3, SetAngle3, SetVelocity4, SetAngle4, SetVelocity5, SetAngle5, SetVelocity6, SetAngle6, SetVelocity7, SetAngle7, SetVelocity8, SetAngle8, SetVelocity9, SetAngle9, SetVelocity10, SetAngle10, SetVelocity11, SetAngle11, SetVelocity12, SetAngle12, SetVelocity13, SetAngle13, SetVelocity14, SetAngle14, SetVelocity15, SetAngle15, SetVelocity16, SetAngle16, SetVelocity17, SetAngle17, SetVelocity18, SetAngle18, SetVelocity19");
-//            lines.addAll(allValues.stream().map(doubleList -> doubleList.stream().map(val -> String.format("%.4f", val)).collect(Collectors.joining(", "))).collect(Collectors.toList()));
-//            Files.write(new File("./res/operationalprofiletest/rawdata/Hist_" + _testID + ".histplot").toPath(), lines);
         }
         catch (IOException exc)
         {
