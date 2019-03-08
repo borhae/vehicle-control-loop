91eb7d0ed5ce67a7c5b42e67c7bfc44fb910a65a
Joachim Haensel <joachim.haensel@hpi.de>
2019-03-08 17:30:47 +0100

    fixed wrong chandigarh data id

--- vrep-control-loop/src/de/joachim/haensel/phd/scenario/experiment/runner/ExperimentRunner.java
+++ vrep-control-loop/src/de/joachim/haensel/phd/scenario/experiment/runner/ExperimentRunner.java
@@ -146,7 +146,7 @@ public class ExperimentRunner
             	List<String> pointsAsString = Files.readAllLines(new File(RES_ROADNETWORKS_DIRECTORY + "Chandigarhpoints_spread.txt").toPath());
                 List<Position2D> positions = pointsAsString.stream().map(string -> new Position2D(string)).collect(Collectors.toList());
 //                runner.run("luebeck_183_max_scattered_targets", 15.0, 120.0, 4.0, 4.3, 1.0, positions, "luebeck-roads.net.xml", "blue");
-                runner.run("luebeck_183_max_scattered_targets", 15.0, 120.0, 4.0, 4.3, 1.0, positions, "chandigarh-roads.net.xml", "blue");
+                runner.run("chandigarh_183_max_scattered_targets", 15.0, 120.0, 4.0, 4.3, 1.0, positions, "chandigarh-roads.net.xml", "blue");
                 runner.tearDown();
             }
             catch (IOException exc)
