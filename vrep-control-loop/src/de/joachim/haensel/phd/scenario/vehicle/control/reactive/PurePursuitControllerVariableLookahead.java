50323ad47109e48c76cdbf9f3b15b0591c417c55
joachim.haensel <joachim.haensel@hpi.de>
2019-03-08 14:58:46 +0100

    last commit of pure pursuit controller before adding adaptiveController

--- vrep-control-loop/src/de/joachim/haensel/phd/scenario/vehicle/control/reactive/PurePursuitControllerVariableLookahead.java
+++ vrep-control-loop/src/de/joachim/haensel/phd/scenario/vehicle/control/reactive/PurePursuitControllerVariableLookahead.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,7 +12,6 @@ import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import de.joachim.haensel.phd.scenario.debug.DebugParams;
-import de.joachim.haensel.phd.scenario.map.IStreetSection;
 import de.joachim.haensel.phd.scenario.math.geometry.Position2D;
 import de.joachim.haensel.phd.scenario.math.geometry.Vector2D;
 import de.joachim.haensel.phd.scenario.vehicle.IActuatingSensing;
