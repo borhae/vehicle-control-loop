14ee9f93a37cec81d088d00828b785cbf0a39b96
Joachim Haensel <joachim.haensel@hpi.de>
2019-07-08 15:32:56 +0200

    merged iterative lookahead approach
    Former-commit-id: 6431c1926b30e345fbfa2775590786744b0d3d12

--- vrep-control-loop/src/de/joachim/haensel/phd/scenario/vehicle/control/reactive/purepuresuitvariable/PurePuresuitTargetProvider.java
+++ vrep-control-loop/src/de/joachim/haensel/phd/scenario/vehicle/control/reactive/purepuresuitvariable/PurePuresuitTargetProvider.java
@@ -19,6 +19,7 @@ public class PurePuresuitTargetProvider
     private Position2D _currentPosition;
     private Vector2D _currentOrientation;
     private TrajectoryElement _currentLookaheadElement;
+    private int _currentClosestElementIndex;
     private Position2D _rearWheelCenterPosition;
 
     public PurePuresuitTargetProvider(TrajectoryBuffer trajectoryBuffer, IActuatingSensing actuatorsSensors)
@@ -26,12 +27,14 @@ public class PurePuresuitTargetProvider
         _trajectoryBuffer = trajectoryBuffer;
         _actuatorsSensors = actuatorsSensors;
         _currentLookaheadElement = null;
+        _currentClosestElementIndex = -1;
     }
 
     public void reset()
     {
         _trajectoryBuffer.clear();
         _currentLookaheadElement = null;
+        _currentClosestElementIndex = -1;
     }
 
     public void loopPrepare()
@@ -48,50 +51,52 @@ public class PurePuresuitTargetProvider
         if (_currentPosition == null)
         {
             System.out.println("Closest Trajectory Element: No reference position available!");
+            return null;
         }
-        else if (_trajectoryBuffer.isEmpty())
+        if (_trajectoryBuffer.isEmpty())
         {
             System.out.println("Closest Trajectory Element: Buffer is empty!");
+            return null;
         }
-        else
+
+        double minDist = Double.POSITIVE_INFINITY;
+        int minDistIdx = Integer.MAX_VALUE;
+        for (int idx = 0; idx < _trajectoryBuffer.size(); idx++)
         {
-            double minDist = Double.POSITIVE_INFINITY;
-            int minDistIdx = Integer.MAX_VALUE;
+            TrajectoryElement curElem = _trajectoryBuffer.get(idx);
+            Vector2D curVector = curElem.getVector();
+            if (Math.toDegrees(Vector2D.computeAngle(curVector, _currentOrientation)) < 120)
+            {
+                double distance = curVector.toLine().distancePerpendicularOrEndpoints(_currentPosition);
+                if (distance < minDist)
+                {
+                    minDist = distance;
+                    minDistIdx = idx;
+                    _currentClosestElementIndex = idx;
+                    result = curElem;
+                }
+            }
+        }
+        if(minDist == Double.POSITIVE_INFINITY)
+        {
+            System.out.println("No element found with ok orientation: going for the closest");
             for (int idx = 0; idx < _trajectoryBuffer.size(); idx++)
             {
                 TrajectoryElement curElem = _trajectoryBuffer.get(idx);
                 Vector2D curVector = curElem.getVector();
-                if (Math.toDegrees(Vector2D.computeAngle(curVector, _currentOrientation)) < 120)
+                double distance = curVector.toLine().distancePerpendicularOrEndpoints(_currentPosition);
+                if (distance < minDist)
                 {
-                    double distance = curVector.toLine().distancePerpendicularOrEndpoints(_currentPosition);
-                    if (distance < minDist)
-                    {
-                        minDist = distance;
-                        minDistIdx = idx;
-                        result = curElem;
-                    }
+                    minDist = distance;
+                    minDistIdx = idx;
+                    _currentClosestElementIndex = idx;
+                    result = curElem;
                 }
             }
-            if(minDist == Double.POSITIVE_INFINITY)
-            {
-                System.out.println("No element found with ok orientation: going for the closest");
-                for (int idx = 0; idx < _trajectoryBuffer.size(); idx++)
-                {
-                    TrajectoryElement curElem = _trajectoryBuffer.get(idx);
-                    Vector2D curVector = curElem.getVector();
-                    double distance = curVector.toLine().distancePerpendicularOrEndpoints(_currentPosition);
-                    if (distance < minDist)
-                    {
-                        minDist = distance;
-                        minDistIdx = idx;
-                        result = curElem;
-                    }
-                }
-            }
-            if (minDistIdx != 0 && minDist != Double.POSITIVE_INFINITY)
-            {
-                _trajectoryBuffer.removeBelowIndex(minDistIdx);
-            }
+        }
+        if (minDistIdx != 0 && minDist != Double.POSITIVE_INFINITY)
+        {
+            _trajectoryBuffer.removeBelowIndex(minDistIdx);
         }
         return result;
     }
@@ -100,58 +105,40 @@ public class PurePuresuitTargetProvider
     {
         if(_currentLookaheadElement == null || !isInRange(_currentLookaheadElement, _rearWheelCenterPosition, lookahead))
         {
-            int bestMatchingSegmentIdx = 0;
-            Map<TrajectoryElement, Integer> matchingElements = new HashMap<>();
-            for(int idx = 0; idx < _trajectoryBuffer.size(); idx++)
+            if(_currentClosestElementIndex == -1)
             {
-                TrajectoryElement curElement = _trajectoryBuffer.get(idx);
-                if(isInRange(curElement, _rearWheelCenterPosition, lookahead))
-                {
-                    matchingElements.put(curElement, idx);
-                }
+                getClosestTrajectoryElement();
             }
-            if(matchingElements.size() > 0)
+            
+            int maxSteps = (int) Math.round(lookahead / 5.0) * 2;           
+            int targetIndex = -1;   
+            
+            
+            //find a fitting trajectory element in front of the vehicle
+            for(int i = _currentClosestElementIndex; i < Math.min(_currentClosestElementIndex + maxSteps, _trajectoryBuffer.size() -1); i++)
             {
-                Predicate<? super Entry<TrajectoryElement, Integer>> isSharpTurn = entry -> Math.toDegrees(Vector2D.computeAngle(entry.getKey().getVector(), _currentOrientation)) < 120;
-                List<Entry<TrajectoryElement, Integer>> matchingOrientation = matchingElements.entrySet().stream().filter(isSharpTurn).collect(Collectors.toList());
-                if(matchingOrientation.size() > 0)
+                if(isInRange(_trajectoryBuffer.get(i), _rearWheelCenterPosition, lookahead))
                 {
-                    bestMatchingSegmentIdx = matchingOrientation.get(0).getValue();
-                }
-                else
-                {
-                    List<Double> elementDegrees = matchingElements.entrySet().stream().map(entry -> Math.toDegrees(Vector2D.computeAngle(entry.getKey().getVector(), _currentOrientation))).collect(Collectors.toList());
-                    System.out.println("Current lookahead: " + lookahead + "Warning: no trajectory element found matching our orientation close enough (120 degrees). Matching elements have these angle differences: " + elementDegrees);
-                    
-                    bestMatchingSegmentIdx = matchingElements.entrySet().iterator().next().getValue();
-                }
-                TrajectoryElement newLookaheadTrajectoryElement = _trajectoryBuffer.get(bestMatchingSegmentIdx);
-                _currentLookaheadElement = newLookaheadTrajectoryElement;
-                //TODO beforehand I kept the old element when the new element was actually moving back on the route 
-//                 only update if the new element has a larger index (is further ahead than current element)
-//                if(_currentLookaheadElement == null)
-//                {
-//                    _currentLookaheadElement = newLookaheadTrajectoryElement;
-//                }
-//                else if(newLookaheadTrajectoryElement.getIdx() > _currentLookaheadElement.getIdx())
-//                {
-//                    _currentLookaheadElement = newLookaheadTrajectoryElement;
-//                }
-//                else
-//                {
-////                    String answer = isInRange(_currentLookaheadElement, _rearWheelCenterPosition, lookahead) ? "yes" : "no";
-////                    System.out.format("Element stays the same since new element would have a lower index then the old one (is that really a reason to keep the old one?). Is old element still in range? The answer is: %s\n", answer);
-//                }
+                    targetIndex = i;
+                    break;
+                }                           
+            }
+            
+            if(targetIndex != -1)
+            {
+                _currentLookaheadElement = _trajectoryBuffer.get(targetIndex);
             }
             else
             {
                 if(_trajectoryBuffer.getCurrentState() != RouteBufferStates.ROUTE_ENDING)
                 {
                     boolean segmentsLeft = _trajectoryBuffer.elementsLeft();
-                    System.out.println("Warning: No matching trajectory element found, waiting for next buffer read");
+                    System.out.println("Warning: No matching trajectory element found, taking the closest trajectory element");
                     System.out.println("Buffersize: " + _trajectoryBuffer.size());
                     String info = segmentsLeft ? "yep" : "nope";
                     System.out.println("Segmentprovider has segments? " + info);
+                    _currentLookaheadElement = _trajectoryBuffer.get(_currentClosestElementIndex);
+                    
                 }
                 else
                 {
@@ -159,6 +146,7 @@ public class PurePuresuitTargetProvider
                 }
             }
         }
+        
         return _currentLookaheadElement;
     }
     
